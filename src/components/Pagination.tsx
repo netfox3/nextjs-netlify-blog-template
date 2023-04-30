9b084fbbbcd6c772b85b86ea422dd51502f4e1fd
netfox3 <52906394+netfox3@users.noreply.github.com>
2023-04-30 22:03:35 +0800

    Update Pagination.tsx

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -27,7 +27,7 @@ export default function Pagination({ current, pages, link }: Props) {
       <style jsx>{`
         ul {
           list-style: none;
-          margin: 3rem 0 5rem 0;
+          margin: 3rem 0 0 0;
           padding: 0;
         }
         li {
