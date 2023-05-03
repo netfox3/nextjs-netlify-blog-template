9aad7520063fc17c1ebbf8d1688d27f7bfdb0f19
netfox3 <52906394+netfox3@users.noreply.github.com>
2023-05-04 07:40:42 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Index() {
             Hi, it's Ash<span className="fancy">.</span>
           </h1>
           <h2>I'm a copywriter, ux writer, content designer, and growth hacker helping startups and entrepreneurs. 
-            Take a look at my <Link href="/posts"><a>favorite projects</a></Link>or
+            Take a look at my <Link href="/posts"><a>favorite projects </a></Link>or
             <Link href="/resume"><a> resume </a></Link>and<Link href="/reviews"><a> client testimonials </a></Link>
             to learn more about me and my working style.</h2>
           <h2><Link href="/contact"><a>Contact me &gt;&gt;</a></Link></h2>
